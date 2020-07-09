374aa778ee160f749eb034fc7b559e0831f64700
piyushP7pravin <piyushpravin1998@gmail.com>
2020-07-10 02:34:48 +0530

    Update Print ith node.py

--- Linked List-1/Print ith node.py
+++ Linked List-1/Print ith node.py
@@ -47,35 +47,59 @@ Sample Output 2 :
     3
     0
 """
+''' 
+    Time Complexity : O(min(i, n)) 
+    Space Complexity : O(1) 
+    Where 'i' is the position of the element to be printed and 'n' being the size of the Singly Linked List
+'''
 
+from sys import stdin
 class Node:
     def __init__(self, data):
         self.data = data
         self.next = None
 
-def ithNode(head, i):
-    count=0
-    while count<i and head!=None:
-        head=head.next
-        count+=1
+def printIthNode(head, i): 
+    position = 0 
+    temp = head 
+    while (temp is not None) and (position < i) : 
+        temp = temp.next 
+        position += 1 
+
+    if temp is not None : 
+        print(temp.data)
+
+def takeInput() : 
+    head = None 
+    tail = None
+
+    datas = list(map(int, stdin.readline().rstrip().split(" ")))
+
+    i = 0 
+    while (i < len(datas)) and (datas[i] != -1) : 
+        data = datas[i] 
+        newNode = Node(data)
+
+        if head is None : 
+            head = newNode 
+            tail = newNode 
+        else : 
+            tail.next = newNode 
+            tail = newNode
+
+        i += 1 
     return head
 
-def ll(arr):
-    if len(arr)==0:
-        return None
-    head = Node(arr[0])
-    last = head
-    for data in arr[1:]:
-        last.next = Node(data)
-        last = last.next
-    return head
+def printLinkedList(head) : 
+    while head is not None : 
+        print(head.data, end = " ") 
+        head = head.next 
+    print()
 
-# Main
-# Read the link list elements including -1
-arr=list(int(i) for i in input().strip().split(' '))
-# Create a Linked list after removing -1 from list
-l = ll(arr[:-1])
-i=int(input())
-node = ithNode(l, i)
-if node:
-    print(node.data)
+#main 
+t = int(stdin.readline().rstrip()) 
+while t > 0 : 
+    head = takeInput() 
+    i = int(stdin.readline().rstrip()) 
+    printIthNode(head, i) 
+    t -= 1

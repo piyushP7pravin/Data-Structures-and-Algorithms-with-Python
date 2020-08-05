dabdad4c112ee1376753393640b1bb56a142460e
piyushP7pravin <piyushpravin1998@gmail.com>
2020-08-05 21:18:27 +0530

    Update Preorder Binary Tree.py

--- Binary Trees - 1/Preorder Binary Tree.py
+++ Binary Trees - 1/Preorder Binary Tree.py
@@ -15,3 +15,57 @@ Pre-order traversal is: Root LeftChild RightChild
 #### Sample Output :
     8 3 1 6 4 7 10 14 13
 """
+import queue
+class BinaryTreeNode:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+def preOrder(root):
+
+    if root is None:
+        return
+
+    print(root.data,end=" ")
+
+    preOrder(root.left)
+    preOrder(root.right)
+
+def buildLevelTree(levelorder):
+
+    index = 0
+    length = len(levelorder)
+    if length<=0 or levelorder[0]==-1:
+        return None
+
+    root = BinaryTreeNode(levelorder[index])
+
+    index += 1
+    q = queue.Queue()
+    q.put(root)
+
+    while not q.empty():
+        currentNode = q.get()
+        leftChild = levelorder[index]
+
+        index += 1
+        if leftChild != -1:
+            leftNode = BinaryTreeNode(leftChild)
+            currentNode.left =leftNode
+            q.put(leftNode)
+
+        rightChild = levelorder[index]
+
+        index += 1
+        if rightChild != -1:
+            rightNode = BinaryTreeNode(rightChild)
+            currentNode.right =rightNode
+            q.put(rightNode)
+            
+    return root
+
+# Main
+levelOrder = [int(i) for i in input().strip().split()]
+root = buildLevelTree(levelOrder)
+preOrder(root)
